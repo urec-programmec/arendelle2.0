c7dbe207728cef57a948d919bdbbef55afce0ff0
Sergey Prostokishin <sepa33126@gmail.com>
2022-01-13 12:20:30 +0300

    #1. Unlink initially creating db

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ python3 -m venv venv
 source venv/bin/activate
 PATH="/Library/PostgreSQL/13/bin:$PATH"
 pip3 install -r requirements.txt
-cd db
-chmod +x dbinit.sh
-./dbinit.sh
+#cd db
+#chmod +x dbinit.sh
+#./dbinit.sh
